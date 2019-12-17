e1822be328e5fe3aa6003486a2660cdd58a8377a
jeremy.favre <jeremy.favre@etu.hesge.ch>
2019-12-17 12:09:56 +0100

    Fix search FAB and change icon for logout close #22

--- app/src/main/java/ch/hes/master/mobopproject/MainActivity.kt
+++ app/src/main/java/ch/hes/master/mobopproject/MainActivity.kt
@@ -3,30 +3,21 @@ package ch.hes.master.mobopproject
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.NavDeepLinkBuilder
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import ch.hes.master.mobopproject.data.Auth
-import ch.hes.master.mobopproject.data.User
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.nav_header_main.view.*
-import java.util.zip.Inflater
 
 
 class MainActivity : AppCompatActivity(), OnListFragmentInteractionListener {
@@ -41,15 +32,13 @@ class MainActivity : AppCompatActivity(), OnListFragmentInteractionListener {
         setSupportActionBar(toolbar)
 
         val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-            //view.findNavController().navigate(ListMoviesFragmentDirections.actionListMoviesFragmentToSearchFragment())
-        }
-
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
+
+        fab.setOnClickListener { view ->
+            navController.navigate(R.id.searchFragment)
+        }
 
         appBarConfiguration = AppBarConfiguration(
             setOf(
